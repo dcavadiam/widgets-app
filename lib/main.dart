fb90015ff8ced4fc9c8272383d9a27a147e90a43
dcavadia <dcavadia@uninorte.edu.co>
2024-02-14 09:18:03 -0500

    config basic routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/theme/app_theme.dart';
-import 'package:widgets_app/presentation/screens/home_screen.dart';
+import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
+import 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
+import 'package:widgets_app/presentation/screens/home/home_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -17,6 +19,10 @@ class MainApp extends StatelessWidget {
       home: const Scaffold(
         body: Center(child: HomeScreen()),
       ),
+      routes: {
+        '/buttons': (context) => const ButtonsScreen(),
+        '/cards': (context) => const CardsScreen(),
+      },
     );
   }
 }
