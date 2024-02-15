374a6b77ae6742f4a66fd27f7288cfe2599561a6
dcavadia <dcavadia@uninorte.edu.co>
2024-02-15 15:49:53 -0500

    created screens and update barrel

---
+++ lib/presentation/screens/infinite_scroll/infinity_scroll_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class InfinityScrollScreen extends StatelessWidget {
+  static const String routeName = 'infinity_scroll_screen';
+
+  const InfinityScrollScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
